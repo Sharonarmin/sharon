6c5570046729524e875701fb86b5d30a0eef54dc
unknown <sharonarmin84@gmail.com>
2022-07-19 23:10:41 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+        var menuBtn = document.getElementById("menuBtn")
+        var sideNav = document.getElementById("sideNav")
+        var menu = document.getElementById("menu")
+        sideNav.style.right == "-250px";
+        menuBtn.onclick = function() {
+            if (sideNav.style.right == "-250px") {
+                sideNav.style.right = "0";
+            } else {
+                sideNav.style.right = "-250px";
+                
+            }
+        }
+
+        $(document).ready(function() {
+
+            $("a").on('click', function(event) {
+
+                if (this.hash !== "") {
+
+                    event.preventDefault();
+                    var hash = this.hash;
+
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 800, function() {
+                        window.location.hash = hash;
+                    });
+                }
+            });
+        });
+
+
+// bg animation
+
+
+
+    VANTA.BIRDS({
+    el: ".hero-page",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xf6f6f6,
+    color1: 0x969699,
+    color2:  0x1e1e22,
+    colorMode: "lerp",
+    birdSize: 1.40,
+    wingSpan: 30.00,
+    quantity: 2.00
+    })
+
+
+
+
+// slider
